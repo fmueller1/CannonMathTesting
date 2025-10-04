52cc6f38159a4d05f7c672960522c74c382a217e
Felix <felixmmueller06@gmail.com>
2025-10-04 13:57:49 -0700

    stuffs

--- src/Derivative.java
+++ src/Derivative.java
@@ -1,13 +1,25 @@
 public class Derivative{
     public static Vector2D takePartialDerivativeA(Differentiand d, Vector2D in, double da){
-        double compA = (d.f(new Vector2D(in.x+da, in.y)).x - d.f(in).x)/ da;
-        double compB = (d.f(new Vector2D(in.x+da, in.y)).y - d.f(in).y)/ da;
+        double compA = (d.f(new Vector2D(in.x+da, in.y)).x - d.f(in).x);
+        double compB = (d.f(new Vector2D(in.x+da, in.y)).y - d.f(in).y);
+        if(compA != 0){
+            compA /= da;
+        }
+        if(compB != 0){
+            compB /= da;
+        }
 
         return new Vector2D(compA, compB);
     }
     public static Vector2D takePartialDerivativeB(Differentiand d, Vector2D in, double db){
-        double compA = (d.f(new Vector2D(in.x, in.y+db)).x - d.f(in).x)/ db;
-        double compB = (d.f(new Vector2D(in.x, in.y+db)).y - d.f(in).y)/ db;
+        double compA = (d.f(new Vector2D(in.x, in.y+db)).x - d.f(in).x);
+        double compB = (d.f(new Vector2D(in.x, in.y+db)).y - d.f(in).y);
+        if(compA != 0){
+            compA /= db;
+        }
+        if(compB != 0){
+            compB /= db;
+        }
 
         return new Vector2D(compA, compB);
     }
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@
 public class Main {
     public static void main(String[] args) {
         double delta = 0.0001;
-        Vector2D inpVector = new Vector2D(  0, 0);
-        Vector2D target = new Vector2D(0, 0);
+        Vector2D inpVector = new Vector2D(  1, 1);
+        Vector2D target = new Vector2D(10, 10);
         inpVector.convertToRads();
         target.convertToRads();
 
