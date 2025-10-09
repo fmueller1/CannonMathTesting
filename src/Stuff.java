795d80b8549f33d05566b05fb6f2546eb92d8e6d
Felix <felixmmueller06@gmail.com>
2025-10-08 17:53:34 -0700

    stuffs

--- src/Stuff.java
+++ src/Stuff.java
@@ -3,25 +3,24 @@ public class Stuff implements Differentiand{
     double distanceToSurface = 0.375;
     double windChargeExplosionRadius = 1;
     double windChargeExplosionPower = 1;
-    double windChargeDetonationSurfaceOffset = 0.25;
+    double windChargeDetonationSurfaceOffset = -0.25;
     double initialTntXOffset = 0;
     double initialTntYOffset = 0.25;
     double initialTntZOffset = 0;
     double initialArrowXOffset = 0;
     double initialArrowYOffset = 0;
     double initialArrowZOffset = 0;
-    double S = -1;
+    double flipYaw = 1;
     double Pw = 1;
-    double flipYaw = 0;
+    double correctionCoef = 0;
 
     public Vector2D f(Vector2D in){
-        double Xi = distanceToSurface;
+
+        double Xi = flipYaw * distanceToSurface;
         double Yi = distanceToSurface * Math.abs(1.0/Math.cos(in.x)) * Math.tan(-in.y);
         double Zi = distanceToSurface * Math.tan(in.x);
 
-        double M = (S+1)/2.0;
-
-        double Xf1 = Xi + initialTntXOffset + S * windChargeDetonationSurfaceOffset;
+        double Xf1 = Xi + initialTntXOffset + flipYaw * windChargeDetonationSurfaceOffset;
         double Yf1 = Yi + initialTntYOffset;
         double Zf1 = Zi + initialTntZOffset;
 
@@ -34,11 +33,24 @@ public class Stuff implements Differentiand{
         double Zf2 = Zf1 + initialArrowZOffset/P;
         P = (windChargeExplosionRadius - Df) * Pw;
 
-        double yawComp = 0;
-        double pitchComp = -Math.atan(Yf2/Xf2);
+        Vector2D tempVec = new Vector2D(Zf2, Xf2);
+        if(tempVec.getQuadrant() == 2){
+            correctionCoef = 1;
+        }
+        else if(tempVec.getQuadrant() == 3){
+            correctionCoef = -1;
+        } else{
+            correctionCoef = 0;
+        }
+
+        double yawComp = clamp(0);
+        double pitchComp = clamp(-Math.atan(Yf2/Xf2) + Math.PI * correctionCoef);
         Vector2D outVector = new Vector2D(yawComp, pitchComp);
         outVector.convertToDegrees();
-//        System.out.println(outVector);
         return outVector;
+    }
+
+    double clamp(double x){
+        return x % (2*Math.PI) - Math.PI;
     }
 }
