45cba3bad6b65172cffa1e4d5a6c3bbeaec8a2a4
Felix <felixmmueller06@gmail.com>
2025-10-15 20:26:26 -0700

    redid math. I have no idea what's goin on anymore

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
     public static void main(String[] args) {
         double delta = 0.0001;
-        Vector2D inpVector = new Vector2D(  180, -180);
+        Vector2D inpVector = new Vector2D(  -96, 36);
         Vector2D target = new Vector2D(10, 10);
         inpVector.convertToRads();
         target.convertToRads();
--- src/Stuff.java
+++ src/Stuff.java
@@ -1,10 +1,10 @@
 public class Stuff implements Differentiand{
 
-    int cannonRotation = 1;
-    double Dsx = 0.375;
-    double Rw = 1.1;
-    double Pw = 1;
-    double Os = -0.25;
+    int cannonOrientation = 1;
+    double Dsx = 0.375; // x distance from surface
+    double Rw = 1.1; //
+    double Pw = 1; // wind-charge power
+    double Os = -0.25; //
     double Oix = 0;
     double Oiy = 0.25;
     double Oiz = 0;
@@ -14,8 +14,8 @@ public class Stuff implements Differentiand{
 
     public Vector2D f(Vector2D in) {
 
-        double rotatedYaw = rotateAxis(in.x, cannonRotation);
-        double directionOfCollisionSurface = -Math.abs(Math.sin(rotateAxis(in.x, cannonRotation-1)))/Math.sin(rotateAxis(in.x, cannonRotation-1));
+        double rotatedYaw = rotateAxis(in.x, cannonOrientation);
+        double directionOfCollisionSurface = -Math.abs(Math.sin(rotateAxis(in.x, cannonOrientation -1)))/Math.sin(rotateAxis(in.x, cannonOrientation -1));
 
         double Xi = directionOfCollisionSurface*Dsx;
         double Yi = Dsx * Math.abs(1.0/Math.cos(rotatedYaw))*Math.tan(-in.y);
@@ -27,11 +27,15 @@ public class Stuff implements Differentiand{
 
         double Df = pythagoreanTheorem(Xf1, Yf1, Zf1);
 
-        double P = (Rw-Df)*Pw;
+        double P = (Rw-Df)*Pw; // effective wind-charge power
 
         double Xf2=Xf1+Xa/P;
         double Yf2=Yf1+Ya/P;
         double Zf2=Zf1+Ya/P;
+
+        System.out.println(Xf2);
+        System.out.println(Yf2);
+        System.out.println(Zf2);
 
         double yawComp = 0;
         double pitchComp = 0;
