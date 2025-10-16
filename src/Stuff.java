b640b611411ccf25de832800079f56dfe83a37be
Felix <felixmmueller06@gmail.com>
2025-10-15 21:01:44 -0700

    aaaaaa

--- src/Stuff.java
+++ src/Stuff.java
@@ -1,37 +1,37 @@
 public class Stuff implements Differentiand{
 
     int cannonOrientation = 1;
-    double Dsx = 0.375; // x distance from surface
-    double Rw = 1.1; //
-    double Pw = 1; // wind-charge power
+    double surfaceDistance = 0.375; // x distance from surface
+    double windChargeRange = 1.1; //
+    double totalWindChargePower = 1; // wind-charge power
     double Os = -0.25; //
-    double Oix = 0;
-    double Oiy = 0.25;
-    double Oiz = 0;
-    double Xa = 0;
-    double Ya = 0;
-    double Za = 0;
+    double initialTNTXOffset = 0;
+    double initialTNTYOffset = 0.25;
+    double initialTNTZOffset = 0;
+    double initialArrowXOffset = 0;
+    double initialArrowYOffset = 0;
+    double initialArrowZOffset = 0;
 
     public Vector2D f(Vector2D in) {
 
-        double rotatedYaw = rotateAxis(in.x, cannonOrientation);
-        double directionOfCollisionSurface = -Math.abs(Math.sin(rotateAxis(in.x, cannonOrientation -1)))/Math.sin(rotateAxis(in.x, cannonOrientation -1));
+        double relativeYaw = rotateAxis(in.x, cannonOrientation);
+        double directionOfCollisionSurface = getDirrectionOfCollisionSurface(relativeYaw);
 
-        double Xi = directionOfCollisionSurface*Dsx;
-        double Yi = Dsx * Math.abs(1.0/Math.cos(rotatedYaw))*Math.tan(-in.y);
-        double Zi = Dsx * Math.tan(rotatedYaw);
+        double Xi = directionOfCollisionSurface* surfaceDistance;
+        double Yi = surfaceDistance * Math.abs(1.0/Math.cos(relativeYaw))*Math.tan(-in.y);
+        double Zi = surfaceDistance * Math.tan(relativeYaw);
 
-        double Xf1 = Xi+Oix+directionOfCollisionSurface*Os;
-        double Yf1 = Yi+Oiy;
-        double Zf1 = Zi+Oiz;
+        double Xf1 = Xi+ initialTNTXOffset +directionOfCollisionSurface*Os;
+        double Yf1 = Yi+ initialTNTYOffset;
+        double Zf1 = Zi+ initialTNTZOffset;
 
         double Df = pythagoreanTheorem(Xf1, Yf1, Zf1);
 
-        double P = (Rw-Df)*Pw; // effective wind-charge power
+        double P = (windChargeRange -Df)* totalWindChargePower; // effective wind-charge power
 
-        double Xf2=Xf1+Xa/P;
-        double Yf2=Yf1+Ya/P;
-        double Zf2=Zf1+Ya/P;
+        double Xf2=Xf1+ initialArrowXOffset /P;
+        double Yf2=Yf1+ initialArrowYOffset /P;
+        double Zf2=Zf1+ initialArrowYOffset /P;
 
         System.out.println(Xf2);
         System.out.println(Yf2);
@@ -45,7 +45,19 @@ public class Stuff implements Differentiand{
     double pythagoreanTheorem(double a, double b, double c){
         return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2));
     }
+
     double rotateAxis(double val, int rot){
         return val + Math.PI/2 * rot;
+    }
+
+    double getCannonRelativeYaw(double val){
+        return rotateAxis(val, cannonOrientation);
+    }
+
+    double getDirrectionOfCollisionSurface(double yaw){
+        if (yaw > 0){
+            return -1;
+        }
+        return 1;
     }
 }
