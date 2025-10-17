c1205a675b514c0301c860023f7e62b5cc05b095
Felix <felixmmueller06@gmail.com>
2025-10-17 16:57:31 -0700

    variable name changes beccause wesley cannot understad letters

--- src/RREF.java
+++ src/RREF.java
@@ -4,31 +4,31 @@ public class RREF {
 
         Vector2D differanceVector = target.subtract(inpVector);
 
-        Vector2D partialDerivativeA = Derivative.takePartialDerivativeA(Model, inpVector, delta);
-        Vector2D partialDerivativeB = Derivative.takePartialDerivativeB(Model, inpVector, delta);
+        Vector2D partialDerivativeY = Derivative.takePartialDerivativeA(Model, inpVector, delta);
+        Vector2D partialDerivativeP = Derivative.takePartialDerivativeB(Model, inpVector, delta);
 
-        double yawWeight = calcYawWeight(partialDerivativeA, partialDerivativeB, differanceVector);
-        double pitchWeight = calcPitchWeight(partialDerivativeA, partialDerivativeB, differanceVector);
+        double yawWeight = calcYawWeight(partialDerivativeY, partialDerivativeP, differanceVector);
+        double pitchWeight = calcPitchWeight(partialDerivativeY, partialDerivativeP, differanceVector);
 
-        double weightedYaw = partialDerivativeA.x * yawWeight + partialDerivativeB.x * pitchWeight;
-        double weightedPitch = partialDerivativeA.y * yawWeight + partialDerivativeB.y * pitchWeight;
+        double weightedYaw = partialDerivativeY.x * yawWeight + partialDerivativeP.x * pitchWeight;
+        double weightedPitch = partialDerivativeY.y * yawWeight + partialDerivativeP.y * pitchWeight;
 
         Vector2D correctionVector = new Vector2D(weightedYaw, weightedPitch);
         return inpVector.add(correctionVector);
     }
 
-    static double calcYawWeight(Vector2D partialDerivativeA, Vector2D partialDerivativeB, Vector2D differanceVector){
-        double yawWeightNumerator = (-partialDerivativeB.x * differanceVector.y + differanceVector.x * partialDerivativeB.y);
-        double yawWeightDenominator = (partialDerivativeA.x * partialDerivativeB.y - partialDerivativeB.x * partialDerivativeA.y);
+    static double calcYawWeight(Vector2D partialDerivativeY, Vector2D partialDerivativeP, Vector2D differanceVector){
+        double yawWeightNumerator = (-partialDerivativeP.x * differanceVector.y + differanceVector.x * partialDerivativeP.y);
+        double yawWeightDenominator = (partialDerivativeY.x * partialDerivativeP.y - partialDerivativeP.x * partialDerivativeY.y);
         if(yawWeightDenominator == 0){
             return 0;
         }
         return yawWeightNumerator/yawWeightDenominator;
     }
 
-    static double calcPitchWeight(Vector2D partialDerivativeA, Vector2D partialDerivativeB, Vector2D differanceVector){
-        double pitchWeightNumerator = (partialDerivativeA.x * differanceVector.y - differanceVector.x * partialDerivativeA.y);
-        double pitchWeightDenominator = (partialDerivativeA.x * partialDerivativeB.y - partialDerivativeB.x * partialDerivativeA.y);
+    static double calcPitchWeight(Vector2D partialDerivativeY, Vector2D partialDerivativeP, Vector2D differanceVector){
+        double pitchWeightNumerator = (partialDerivativeY.x * differanceVector.y - differanceVector.x * partialDerivativeY.y);
+        double pitchWeightDenominator = (partialDerivativeY.x * partialDerivativeP.y - partialDerivativeP.x * partialDerivativeY.y);
         if(pitchWeightDenominator == 0){
             return 0;
         }
