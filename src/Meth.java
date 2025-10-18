6917ad0775b84cd3d1e403017162070b1e7d4117
Felix <felixmmueller06@gmail.com>
2025-10-17 17:22:50 -0700

    .

--- src/Meth.java
+++ src/Meth.java
@@ -1,23 +1,39 @@
 
 public class Meth {
-    public static Vector2D reducedRowEchelonForm(Vector2D inpVector, Vector2D target, Differentiand Model, double delta){
 
-        Vector2D differanceVector = target.subtract(inpVector);
+    final static double acceptableError = 0.1;
+
+    public static Vector2D targetInput(Vector2D inpVector, Vector2D target, Differentiand Model, double delta){
+        int iterations = 10;
+        Vector2D out = inpVector;
+        for(int i = 0; i < iterations; i++){
+            Vector2D error = target.subtract(Model.f(out));
+            if(error.getMagnitude() < acceptableError){
+                return out;
+            }
+            out = aproxTargetInput(out, target, Model, delta);
+        }
+        return out;
+    }
+
+
+    public static Vector2D aproxTargetInput(Vector2D inpVector, Vector2D target, Differentiand Model, double delta){
+
+
+        Vector2D differanceVector = target.subtract(Model.f(inpVector));
 
         Vector2D partialDerivativeY = Derivative.takePartialDerivativeA(Model, inpVector, delta);
         Vector2D partialDerivativeP = Derivative.takePartialDerivativeB(Model, inpVector, delta);
 
-        double yawWeight = calcYawWeight(partialDerivativeY, partialDerivativeP, differanceVector);
-        double pitchWeight = calcPitchWeight(partialDerivativeY, partialDerivativeP, differanceVector);
+        double yawCorrection = calcYawCorrection(partialDerivativeY, partialDerivativeP, differanceVector);
+        double pitchCorrection = calcPitchCorrection(partialDerivativeY, partialDerivativeP, differanceVector);
 
-        double weightedYaw = partialDerivativeY.x * yawWeight + partialDerivativeP.x * pitchWeight;
-        double weightedPitch = partialDerivativeY.y * yawWeight + partialDerivativeP.y * pitchWeight;
+        Vector2D correctionVector = new Vector2D(yawCorrection, pitchCorrection);
 
-        Vector2D correctionVector = new Vector2D(weightedYaw, weightedPitch);
         return inpVector.add(correctionVector);
     }
 
-    static double calcYawWeight(Vector2D partialDerivativeY, Vector2D partialDerivativeP, Vector2D differanceVector){
+    static double calcYawCorrection(Vector2D partialDerivativeY, Vector2D partialDerivativeP, Vector2D differanceVector){
         double yawWeightNumerator = (-partialDerivativeP.x * differanceVector.y + differanceVector.x * partialDerivativeP.y);
         double yawWeightDenominator = (partialDerivativeY.x * partialDerivativeP.y - partialDerivativeP.x * partialDerivativeY.y);
         if(yawWeightDenominator == 0){
@@ -26,7 +42,7 @@ public class Meth {
         return yawWeightNumerator/yawWeightDenominator;
     }
 
-    static double calcPitchWeight(Vector2D partialDerivativeY, Vector2D partialDerivativeP, Vector2D differanceVector){
+    static double calcPitchCorrection(Vector2D partialDerivativeY, Vector2D partialDerivativeP, Vector2D differanceVector){
         double pitchWeightNumerator = (partialDerivativeY.x * differanceVector.y - differanceVector.x * partialDerivativeY.y);
         double pitchWeightDenominator = (partialDerivativeY.x * partialDerivativeP.y - partialDerivativeP.x * partialDerivativeY.y);
         if(pitchWeightDenominator == 0){
