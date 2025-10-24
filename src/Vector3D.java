01806f853b59b149c2eed04c189052b4f2992e41
Felix <felixmmueller06@gmail.com>
2025-10-24 15:43:01 -0700

    added Vector3D

---
+++ src/Vector3D.java
@@ -0,0 +1,38 @@
+public class Vector3D {
+    public double x;
+    public double y;
+    public double z;
+
+    public Vector3D(){
+        x = 0;
+        y = 0;
+        z = 0;
+    }
+    public Vector3D(double x, double y, double z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public String toString(){
+        return "(" + x + ", " + y + ", " + z + ")";
+    }
+
+    public Vector2D getPitchYaw(){
+        double yaw = Math.atan2(x, Math.sqrt(Math.pow(z,2)+Math.pow(y,2)));
+        double pitch = y/this.getMagnitude();
+        return new Vector2D(yaw, pitch);
+    }
+    public double getMagnitude(){
+        return Math.sqrt(Math.pow(x, 2)+Math.pow(y, 2)+Math.pow(z, 2));
+    }
+    public Vector3D add(Vector3D other){
+        return new Vector3D(this.x + other.x, this.y + other.y, this.z + other.z);
+    }
+    public Vector3D subtract(Vector3D other){
+        return new Vector3D(this.x - other.x, this.y - other.y, this.z - other.z);
+    }
+    public double dotProduct(Vector3D other){
+        return this.x * other.x + this.y * other.y + this.z + other.z;
+    }
+}
